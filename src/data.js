9510050dde08ce176735a611e7bf5c9e9b46c1ae
Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2024-09-19 12:56:54 -0500

    updated data layout to match charts normally found in teaching

--- src/data.js
+++ src/data.js
@@ -1,188 +1,247 @@
+// export const kana = {
+//   Hiragana: {
+//     あ: {
+//       romaji: 'a',
+//       combo: false,
+//     },
+//     い: {
+//       romaji: 'i',
+//       combo: false,
+//     },
+//     う: {
+//       romaji: 'u',
+//       combo: false,
+//     },
+//     え: {
+//       romaji: 'e',
+//       combo: false,
+//     },
+//     お: {
+//       romaji: 'o',
+//       combo: false,
+//     },
+//     か: {
+//       romaji: 'ka',
+//       combo: false,
+//     },
+//     き: {
+//       romaji: 'ki',
+//       combo: false,
+//     },
+//     く: {
+//       romaji: 'ku',
+//       combo: false,
+//     },
+//     け: {
+//       romaji: 'ke',
+//       combo: false,
+//     },
+//     こ: {
+//       romaji: 'ko',
+//       combo: false,
+//     },
+//     さ: {
+//       romaji: 'sa',
+//       combo: false,
+//     },
+//     し: {
+//       romaji: 'shi',
+//       combo: false,
+//     },
+//     す: {
+//       romaji: 'su',
+//       combo: false,
+//     },
+//     せ: {
+//       romaji: 'se',
+//       combo: false,
+//     },
+//     そ: {
+//       romaji: 'so',
+//       combo: false,
+//     },
+//     た: {
+//       romaji: 'ta',
+//       combo: false,
+//     },
+//     ち: {
+//       romaji: 'chi',
+//       combo: false,
+//     },
+//     つ: {
+//       romaji: 'tsu',
+//       combo: false,
+//     },
+//     て: {
+//       romaji: 'te',
+//       combo: false,
+//     },
+//     と: {
+//       romaji: 'to',
+//       combo: false,
+//     },
+//     な: {
+//       romaji: 'na',
+//       combo: false,
+//     },
+//     に: {
+//       romaji: 'ni',
+//       combo: false,
+//     },
+//     ぬ: {
+//       romaji: 'nu',
+//       combo: false,
+//     },
+//     ね: {
+//       romaji: 'ne',
+//       combo: false,
+//     },
+//     の: {
+//       romaji: 'no',
+//       combo: false,
+//     },
+//     は: {
+//       romaji: 'ｈa',
+//       combo: false,
+//     },
+//     ひ: {
+//       romaji: 'ｈi',
+//       combo: false,
+//     },
+//     ふ: {
+//       romaji: 'fu',
+//       combo: false,
+//     },
+//     へ: {
+//       romaji: 'ｈe',
+//       combo: false,
+//     },
+//     ほ: {
+//       romaji: 'ｈo',
+//       combo: false,
+//     },
+//     ま: {
+//       romaji: 'ma',
+//       combo: false,
+//     },
+//     み: {
+//       romaji: 'mi',
+//       combo: false,
+//     },
+//     む: {
+//       romaji: 'mu',
+//       combo: false,
+//     },
+//     め: {
+//       romaji: 'me',
+//       combo: false,
+//     },
+//     も: {
+//       romaji: 'mo',
+//       combo: false,
+//     },
+//     や: {
+//       romaji: 'ya',
+//       combo: false,
+//     },
+//     ゆ: {
+//       romaji: 'yu',
+//       combo: false,
+//     },
+//     よ: {
+//       romaji: 'yo',
+//       combo: false,
+//     },
+//     ら: {
+//       romaji: 'ra',
+//       combo: false,
+//     },
+//     り: {
+//       romaji: 'ri',
+//       combo: false,
+//     },
+//     る: {
+//       romaji: 'ru',
+//       combo: false,
+//     },
+//     れ: {
+//       romaji: 're',
+//       combo: false,
+//     },
+//     ろ: {
+//       romaji: 'ro',
+//       combo: false,
+//     },
+//     わ: {
+//       romaji: 'wa',
+//       combo: false,
+//     },
+//     を: {
+//       romaji: 'o',
+//       combo: false,
+//     },
+//     ん: {
+//       romaji: 'n',
+//       combo: false,
+//     },
+//     が: {
+//       romaji: 'ga',
+//       combo: false,
+//     },
+//     ぎ: {
+//       romaji: 'gi',
+//       combo: false,
+//     },
+//     ぐ: {
+//       romaji: 'gu',
+//       combo: false,
+//     },
+//     げ: {
+//       romaji: 'ge',
+//       combo: false,
+//     },
+//     ご: {
+//       romaji: 'go',
+//       combo: false,
+//     },
+//     あ: {
+//       romaji: 'a',
+//       combo: false,
+//     },
+//     い: {
+//       romaji: 'i',
+//       combo: false,
+//     },
+//     う: {
+//       romaji: 'u',
+//       combo: false,
+//     },
+//     え: {
+//       romaji: 'e',
+//       combo: false,
+//     },
+//     お: {
+//       romaji: 'o',
+//       combo: false,
+//     },
+//   },
+// };
+
 export const kana = {
-  Hiragana: {
-    あ: {
-      romaji: 'a',
-      combo: false,
+  '': {
+    hiragana: {
+      a: あ,
+      i: い,
+      u: う,
+      e: え,
+      o: お,
     },
-    い: {
-      romaji: 'i',
-      combo: false,
-    },
-    う: {
-      romaji: 'u',
-      combo: false,
-    },
-    え: {
-      romaji: 'e',
-      combo: false,
-    },
-    お: {
-      romaji: 'o',
-      combo: false,
-    },
-    か: {
-      romaji: 'ka',
-      combo: false,
-    },
-    き: {
-      romaji: 'ki',
-      combo: false,
-    },
-    く: {
-      romaji: 'ku',
-      combo: false,
-    },
-    け: {
-      romaji: 'ke',
-      combo: false,
-    },
-    こ: {
-      romaji: 'ko',
-      combo: false,
-    },
-    さ: {
-      romaji: 'sa',
-      combo: false,
-    },
-    し: {
-      romaji: 'shi',
-      combo: false,
-    },
-    す: {
-      romaji: 'su',
-      combo: false,
-    },
-    せ: {
-      romaji: 'se',
-      combo: false,
-    },
-    そ: {
-      romaji: 'so',
-      combo: false,
-    },
-    た: {
-      romaji: 'ta',
-      combo: false,
-    },
-    ち: {
-      romaji: 'chi',
-      combo: false,
-    },
-    つ: {
-      romaji: 'tsu',
-      combo: false,
-    },
-    て: {
-      romaji: 'te',
-      combo: false,
-    },
-    と: {
-      romaji: 'to',
-      combo: false,
-    },
-    な: {
-      romaji: 'na',
-      combo: false,
-    },
-    に: {
-      romaji: 'ni',
-      combo: false,
-    },
-    ぬ: {
-      romaji: 'nu',
-      combo: false,
-    },
-    ね: {
-      romaji: 'ne',
-      combo: false,
-    },
-    の: {
-      romaji: 'no',
-      combo: false,
-    },
-    は: {
-      romaji: 'ｈa',
-      combo: false,
-    },
-    ひ: {
-      romaji: 'ｈi',
-      combo: false,
-    },
-    ふ: {
-      romaji: 'fu',
-      combo: false,
-    },
-    へ: {
-      romaji: 'ｈe',
-      combo: false,
-    },
-    ほ: {
-      romaji: 'ｈo',
-      combo: false,
-    },
-    ま: {
-      romaji: 'ma',
-      combo: false,
-    },
-    み: {
-      romaji: 'mi',
-      combo: false,
-    },
-    む: {
-      romaji: 'mu',
-      combo: false,
-    },
-    め: {
-      romaji: 'me',
-      combo: false,
-    },
-    も: {
-      romaji: 'mo',
-      combo: false,
-    },
-    や: {
-      romaji: 'ya',
-      combo: false,
-    },
-    ゆ: {
-      romaji: 'yu',
-      combo: false,
-    },
-    よ: {
-      romaji: 'yo',
-      combo: false,
-    },
-    ら: {
-      romaji: 'ra',
-      combo: false,
-    },
-    り: {
-      romaji: 'ri',
-      combo: false,
-    },
-    る: {
-      romaji: 'ru',
-      combo: false,
-    },
-    れ: {
-      romaji: 're',
-      combo: false,
-    },
-    ろ: {
-      romaji: 'ro',
-      combo: false,
-    },
-    わ: {
-      romaji: 'wa',
-      combo: false,
-    },
-    を: {
-      romaji: 'o',
-      combo: false,
-    },
-    ん: {
-      romaji: 'n',
-      combo: false,
+    katakana: {
+      a: ア,
+      i: イ,
+      u: ウ,
+      e: エ,
+      o: オ,
     },
   },
 };
