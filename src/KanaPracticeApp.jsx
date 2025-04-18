427b6983a272905d551f5183bbc390048a3a27be
Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2025-04-18 15:23:29 -0500

    Added basic structure of Kana display area and user input.

--- src/KanaPracticeApp.jsx
+++ src/KanaPracticeApp.jsx
@@ -1,11 +1,17 @@
 import { kana } from "./data";
 import Tables from "./components/Tables/Tables";
+import Kana from "./components/Kana/Kana";
 
 const KanaPracticeApp = () => {
   return (
-    <>
-      <Tables kana={kana} />
-    </>
+    <div className='container'>
+      <div className='container__kana'>
+        <Kana />
+      </div>
+      <div className='container__tables'>
+        <Tables kana={kana} />
+      </div>
+    </div>
   );
 };
 
